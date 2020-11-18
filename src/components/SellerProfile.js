9c0bf34f41f3e544285ab48a4de5c3ed422f2abf
ARSHIA SALI <arshiasali@gmail.com>
2020-11-17 22:53:01 -0800

    Shop registration changes

--- src/components/SellerProfile.js
+++ src/components/SellerProfile.js
@@ -5,42 +5,23 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Pool from '../UserPool';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-modal';
-import Zoom from 'react-reveal/Zoom';
+
 
 class SellerProfile extends Component {
   constructor(props) {
       super(props)
     
       this.state = {
-              isRegistered: false,
               email:'',
               firstname:'',
               lastname:'',
               shopname:'',
               shopdescription:'',
-              showRegisterModal:false,
               uuid:''
       }
-      this.registerShop = this.registerShop.bind(this);
-      this.confirmRegistration = this.confirmRegistration.bind(this);
   
   }
-  registerShop(){
-    this.setState({showRegisterModal:true});
-  }
-  confirmRegistration(){
 
-    this.setState({showRegisterModal:false,isRegistered:true});
-    console.log(this.state);
-        /*Axios to save the shop name and shop description to database 
-    Pass UUID, Shop name and Shop description available in state attributes to the API.*/
-
-  }
-  closeModal = () => {
-    this.setState({showRegisterModal:false})
-  }
   componentDidMount(){
     //fetch the seller information
     const user = Pool.getCurrentUser();
@@ -49,13 +30,13 @@ class SellerProfile extends Component {
           if (!err) {
             console.log(session);
             this.setState({email:session.getIdToken().payload["email"]})
-            this.setState({firstname:session.getIdToken().payload["custom:firstname"]})
-            this.setState({lastname:session.getIdToken().payload["custom:lastname"]})
+            this.setState({firstname:session.getIdToken().payload["custom:firstName"]})
+            this.setState({lastname:session.getIdToken().payload["custom:lastName"]})
             this.setState({uuid:session.getAccessToken().payload["username"]})
           } 
         });
       } 
-   /*Axios to fetch whether the shop is registered. if registered, set 'isRegistered' in state as true, else false*/
+  
   }
 
 
@@ -64,88 +45,6 @@ class SellerProfile extends Component {
       return (
           <>
           <NavbarSeller />
-          {!this.state.isRegistered ? (
-                <div className="profile-details">
-                           <Button variant="success"  onClick={this.registerShop}>
-                              REGISTER YOUR SHOP
-                            </Button>
-                            <br />
-                            <br />
-                    <Container>
-                    <h2>My Profile</h2>
-                    <hr />
-                    <Form>
-  <Form.Group as={Row} controlId="formPlaintextFirstName">
-    <Form.Label column sm="2">
-      First Name
-    </Form.Label>
-    <Col sm="10">
-      <Form.Control size="lg" plaintext value={this.state.firstname} />
-    </Col>
-  </Form.Group>
-
-  <Form.Group as={Row} controlId="formPlaintextLastName">
-    <Form.Label column sm="2">
-      Last Name
-    </Form.Label>
-    <Col sm="10">
-      <Form.Control size="lg" plaintext  value={this.state.lastname} />
-    </Col>
-  </Form.Group>
-  <Form.Group as={Row} controlId="formPlaintextEmail">
-    <Form.Label column sm="2">
-      Email
-    </Form.Label>
-    <Col sm="10">
-      <Form.Control size="lg" plaintext  value={this.state.email}/>
-    </Col>
-  </Form.Group>
-
-  
-</Form>
-</Container>
-{this.state.showRegisterModal && <Modal isOpen={true} onRequestClose={this.closeModal}>
-                 <Zoom>
-                     <button className="close-modal" onClick={this.closeModal}>x</button>
-                     <h2 className="center">REGISTER YOUR SHOP</h2>
-
-                     <Form>
-
-                        <Form.Group controlId="formShopName">
-
-                        <Row>
-                        <Col md={{ span: 6, offset: 3 }}><Form.Label>Shop Name</Form.Label></Col>
-                        </Row>
-                        <Row>
-                        <Col md={{ span: 6, offset: 3 }}>
-                        <Form.Control required type="text" placeholder="Enter shopname" value={this.state.shopname}
-                        onChange={event => this.setState({shopname:event.target.value})} /></Col>
-                        </Row>
-                        </Form.Group>
-
-                        <Form.Group controlId="formShpDescription">
-                        <Row>
-                        <Col md={{ span: 6, offset: 3 }}><Form.Label>Shop Description </Form.Label></Col>
-                        </Row>
-                        <Row>
-                        <Col md={{ span: 6, offset: 3 }}>
-                        <Form.Control required type="text" placeholder="Enter shop description" value={this.state.shopdescription}
-                        onChange={event => this.setState({shopdescription:event.target.value})} /></Col>
-                        </Row>
-                        </Form.Group>
-
-                        <Row>
-                            <Col md={{ span: 5, offset: 5 }}>
-                                <Button variant="info" onClick={this.confirmRegistration}>
-                                    Register Shop
-                            </Button></Col>
-                        </Row>
-                          
-                        </Form>
-                  </Zoom> </Modal>}
-                </div>
-                )
-                :
                 <div className="profile-details">
                  <Container>
                  <h2>My Profile</h2>
@@ -194,7 +93,7 @@ class SellerProfile extends Component {
                   </Form.Group>
                   </Form>
                   </Container>
-             </div>}
+             </div>
       </>)
   }
 }
