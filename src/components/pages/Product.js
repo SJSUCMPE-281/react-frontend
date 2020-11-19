24b7805ac4c867b2e278700aa76ad523dab8f7e8
ARSHIA SALI <arshiasali@gmail.com>
2020-11-18 17:43:10 -0800

    Shop View HyperLink

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -7,7 +7,7 @@ import ReactStars from "react-rating-stars-component";
 import { connect } from "react-redux";
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
-
+import { Link } from 'react-router-dom';
 import {
   getProducts,
   getProduct,
@@ -23,8 +23,8 @@ class Product extends Component {
     this.state = {
       product: null,
       reviews: [],
-      rating:'',
-      review:''
+      rating: '',
+      review: ''
     };
   }
   async openModal(product) {
@@ -40,7 +40,7 @@ class Product extends Component {
   }
 
   closeModal = () => {
-    this.setState({ product: null,rating: '',review:''});
+    this.setState({ product: null, rating: '', review: '' });
   };
   componentDidMount() {
     this.props.getProducts();
@@ -53,8 +53,8 @@ class Product extends Component {
     const { product, reviews } = this.state;
     const ratingChanged = (newRating) => {
       console.log(newRating);
-      this.setState({rating:newRating})
-      
+      this.setState({ rating: newRating })
+
     };
     return (
       <div>
@@ -97,44 +97,44 @@ class Product extends Component {
                   {reviews.length === 0 ? (
                     <div>No one has reviewed the product yet!</div>
                   ) : (
-                    <ListGroup>
-                      {reviews.map((review) => (
-                        <ListGroup.Item key={review.reviewId}>
-                          <h2>{review.buyerName}</h2>
-                          <ReactStars
-                            count={5}
-                            size={18}
-                            edit={false}
-                            isHalf={true}
-                            color="gray"
-                            activeColor="#f5b942"
-                            value={review.rating}
-                          />
-                          <Alert variant="dark">{review.review}</Alert>
-                        </ListGroup.Item>
-                      ))}
-                    </ListGroup>
-                  )}
+                      <ListGroup>
+                        {reviews.map((review) => (
+                          <ListGroup.Item key={review.reviewId}>
+                            <h2>{review.buyerName}</h2>
+                            <ReactStars
+                              count={5}
+                              size={18}
+                              edit={false}
+                              isHalf={true}
+                              color="gray"
+                              activeColor="#f5b942"
+                              value={review.rating}
+                            />
+                            <Alert variant="dark">{review.review}</Alert>
+                          </ListGroup.Item>
+                        ))}
+                      </ListGroup>
+                    )}
                   <Form>
                     <hr />
                     <Form.Group controlId="review">
-                    <Form.Label>Add Your Review</Form.Label>
-                    <Form.Control type="text" placeholder="Enter review"  onChange={event => this.setState({review:event.target.value})}/>
-                  </Form.Group>
+                      <Form.Label>Add Your Review</Form.Label>
+                      <Form.Control type="text" placeholder="Enter review" onChange={event => this.setState({ review: event.target.value })} />
+                    </Form.Group>
                     <ReactStars
-                          count={5}
-                          size={18}
-                          edit={true}
-                          isHalf={false}
-                          color="gray"
-                          activeColor="#f5b942"
-                          value={0}
-                          onChange={ratingChanged}
-                        />
-                  <Button variant="warning" onClick={this.addReview}>
-                    Submit Your Review
+                      count={5}
+                      size={18}
+                      edit={true}
+                      isHalf={false}
+                      color="gray"
+                      activeColor="#f5b942"
+                      value={0}
+                      onChange={ratingChanged}
+                    />
+                    <Button variant="warning" onClick={this.addReview}>
+                      Submit Your Review
                   </Button>
-                </Form>
+                  </Form>
                 </div>
               </div>
             </Zoom>{" "}
@@ -159,7 +159,9 @@ class Product extends Component {
                     <img src={product.imageUrl} alt={product.productName}></img>
                     <p>{product.productName}</p>
                   </a>
-                  <h5 className="text-muted">{product.shopName}</h5>
+                  <Link to='/shopview'>
+                    <h5 className="text-muted">{product.shopName}</h5>
+                  </Link>
                   <span>
                     <span className="widthhalf">
                       <ReactStars
