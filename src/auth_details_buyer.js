67699fd31be253b458663bf7f04ecc2f49c54218
ARSHIA SALI <arshiasali@gmail.com>
2020-11-29 17:50:13 -0800

    updated redirection url

--- src/auth_details_buyer.js
+++ src/auth_details_buyer.js
@@ -2,8 +2,8 @@ const auth_details_buyer = {
     ClientId : process.env.REACT_APP_CLIENT_ID, // Your client id here
     AppWebDomain : process.env.REACT_APP_WEB_DOMAIN,
     TokenScopesArray : ['email', 'profile','openid'], // e.g.['phone', 'email', 'profile','openid', 'aws.cognito.signin.user.admin'],
-    RedirectUriSignIn : 'http://localhost:3000/buyerhome',
-    RedirectUriSignOut : 'http://localhost:3000/',
+    RedirectUriSignIn : 'https://saasshoppe.co/buyerhome',
+    RedirectUriSignOut : 'https://saasshoppe.co/',
     IdentityProvider : 'Facebook', // e.g. 'Facebook',
     UserPoolId : process.env.REACT_APP_USER_POOL_ID, // Your user pool id here
    };
